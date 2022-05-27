d65900a95036559d22b314a29980fddf0229f2f5
walid23W <95526626+walid23W@users.noreply.github.com>
2022-05-27 03:33:47 +0100

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,6 @@ const express = require('express')
 const actions = require('../methods/actions')
 const router = express.Router()
 
-router.get('/', (req, res) => {
-    res.send('Hello World')
-})
-
-router.get('/dashboard', (req, res) => {
-    res.send('Dashboard')
-})
-
 //@desc Adding new user
 //@route POST /adduser
 router.post('/signup', actions.signup)
